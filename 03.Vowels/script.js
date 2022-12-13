a86d4d4466d0d52333be2749c06f6a0a60fb58e2
Matthieu <reungoat.matthieu@gmail.com>
2022-12-13 12:09:55 +0100

    exercice 3 : Vowels completed

--- 03.Vowels/script.js
+++ 03.Vowels/script.js
@@ -1,17 +1,23 @@
-function getVowelsNumber(txt) {
-
-}
-
+const getVowelsNumber = (str) => {
+  let totalVowel = 0;
+  [...str].forEach((vowel) => {
+    if (vowel === "a") totalVowel = totalVowel + 1;
+    if (vowel === "e") totalVowel = totalVowel + 1;
+    if (vowel === "i") totalVowel = totalVowel + 1;
+    if (vowel === "o") totalVowel = totalVowel + 1;
+    if (vowel === "u") totalVowel = totalVowel + 1;
+    if (vowel === "y") totalVowel = totalVowel + 1;
+  });
+  return totalVowel;
+};
 
 /* ÉNONCÉ 📚 */
 
-
 /* Créez un programme qui retourne le nombre de voyelles dans une chaîne de caractères. */
-
 
 /* Tests à passer 🧪 */
 
-console.log(getVowelsNumber("jdhqgdqsghdakzejamazemlqksd"));               // 5
-console.log(getVowelsNumber("Lorem ipsum dolor sit amet consectetur."));   // 13
-console.log(getVowelsNumber("L’imagination gouverne le monde."));          // 13
-console.log(getVowelsNumber("zzzZZZzz"));                                  // 0
+console.log(getVowelsNumber("jdhqgdqsghdakzejamazemlqksd")); // 5
+console.log(getVowelsNumber("Lorem ipsum dolor sit amet consectetur.")); // 13
+console.log(getVowelsNumber("L’imagination gouverne le monde.")); // 13
+console.log(getVowelsNumber("zzzZZZzz")); // 0
